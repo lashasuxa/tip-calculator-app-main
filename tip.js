cb881669db6b2e92d630838cb1aa4f9f79bf4e12
lashasuxa <lashasukhashvili1995@gmail.com>
2023-01-16 00:05:18 -0500

    renewed js , and files

--- tip.js
+++ tip.js
@@ -1,59 +1,111 @@
-let tipAmountElement=document.querySelector('#tip-amount');
-let totalAmountElement=document.querySelector('#total-amount');
+"use strict";
 
+const billInput = document.querySelector("#bill-input");
+const customTipInput = document.querySelector(".custom-tip-input");
+const numberOfPeopleInput = document.querySelector("#number-of-people-input");
+const tipAmountValue = document.querySelector(".tip-amount-value");
+const totalValue = document.querySelector(".total-value");
+const btnReset = document.querySelector(".btn-reset");
+const selectTipBtns = document.querySelectorAll(".select-tip-percentage");
 
+btnReset.addEventListener("click", (event) => {
+  // Reset the inputs and values
+  billInput.value = "0";
+  customTipInput.value = "";
+  numberOfPeopleInput.value = "1";
+  tipAmountValue.innerHTML = "$0";
+  totalValue.innerHTML = "$0";
 
-let billInput =document.querySelector('.inp');
-let peopleInput =document.querySelector('#people-input');
-let tipButtons =document.querySelectorAll('.tipbutton');
-let billValue=0;
-let peopleValue=0;
-let percentValue=0;
-let tipAmount=0;
-let totalAmount=0;
+  // Reset the tip percentage btns
+  selectTipBtns.forEach((tipBtn) => {
+    tipBtn.classList.remove("active");
+  });
 
+  // Set 15% as the default tip percentage
+  selectTipBtns[2].classList.add("active");
+});
 
+// Make the tip percentage btns clickable
+selectTipBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    selectTipBtns.forEach((tipBtn) => {
+      tipBtn.classList.remove("active");
+    });
 
-billInput.addEventListener('input',function(){
-    let value= billInput.value
-    billValue=Number(value)
-    
-})
+    if (event.target.classList.contains("custom-tip-input")) {
+      event.target.parentElement.classList.add("active");
+    } else {
+      event.target.classList.add("active");
+    }
 
-peopleInput.addEventListener('input',function(){
-    let value=peopleInput.value
-    peopleValue=Number(value)
-       
-})
+    calculateTip();
+  });
+});
 
+const calculateTip = () => {
+  const billValue = parseFloat(billInput.value);
+  const numberOfPeople = parseFloat(numberOfPeopleInput.value);
+  const customTipActive = document.querySelector(".select-tip-custom.active");
+  let tipPercentage = parseInt(
+    document.querySelector(".select-tip-percentage.active").dataset.percentage
+  );
 
-for (let i=0 ; i<tipButtons.length ; i++ ) {
-    let btn=tipButtons[i];
-    btn.addEventListener('click',function(){
-        let value= btn.value ;
-        percentValue=Number(value)
-        calculate()
-    })
-}
+  if (customTipActive) {
+    tipPercentage = parseFloat(
+      document.querySelector(".custom-tip-input").value
+    );
+  }
 
-function calculate () {
-    tipAmount = billValue*percentValue/100/peopleValue;
-    totalAmount = billValue/peopleValue +tipAmount;
-    console.log(tipAmount,totalAmount);
-    tipAmountElement.innerHTML='$'+tipAmount;
-    totalAmountElement.innerHTML='$'+totalAmount;
-    
+  const totalAmount = parseFloat((tipPercentage / 100) * billValue).toFixed(2);
+  const tipAmount = parseFloat(totalAmount / numberOfPeople).toFixed(2);
+  const actualTotalAmount = parseFloat(
+    (billValue + parseFloat(totalAmount)) / numberOfPeople
+  ).toFixed(2);
 
-     
-    
-}
+  tipAmountValue.innerHTML = `$${tipAmount}`;
+  totalValue.innerHTML = `$${actualTotalAmount}`;
+};
 
+const isNumber = (value) => {
+  // Allow Exceptions
+  if (
+    value === "Backspace" ||
+    value === "ArrowLeft" ||
+    value === "ArrowRight" ||
+    value === "."
+  ) {
+    return true;
+  }
 
+  const regex = /^[0-9]+$/;
 
+  return regex.test(value);
+};
 
+// Calculate the tip
+billInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+customTipInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+numberOfPeopleInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
 
-
-
-
-
-
+// Prevent alphabetical characters from being entered
+billInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+customTipInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+numberOfPeopleInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
